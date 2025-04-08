dff27ab40183a1cef129ef7bf7a9f79e9d7b7ea1
tay phan <tayphan13579@gmail.com>
2025-04-08 23:02:04 +0700

    feat:logout,token

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 let { CreateErrorRes } = require("./utils/responseHandler");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -13,12 +14,12 @@ var app = express();
 
 mongoose.connect("mongodb://localhost:27017/S5");
 mongoose.connection.on("connected", () => {
-    console.log("connected");
+  console.log("connected");
 });
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -43,15 +44,23 @@ app.use("/likes", require("./routes/like"));
 app.use("/transactionHistory", require("./routes/transactionHistory"));
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-    CreateErrorRes(res, err.message, err.status || 500);
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  CreateErrorRes(res, err.message, err.status || 500);
 });
+
+// Cors
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 module.exports = app;
