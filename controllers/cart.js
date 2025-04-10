e093dbe97b911ba39b25e2832303dbabfd5cf8b1
DangAnhVu <danganhvupro3z@gmail.com>
2025-04-10 20:45:53 +0700

    fix:carts-product

--- controllers/cart.js
+++ controllers/cart.js
@@ -4,37 +4,44 @@ var productModel = require("../schemas/products");
 module.exports = {
     // 1. Lấy tất cả cart
     GetAllCart: async function () {
-        return await cartModel.find().populate("user").populate("items.product");
+        return await cartModel
+            .find()
+            .populate("user")
+            .populate("items.product");
     },
 
     // 2. Lấy cart theo ID
     GetCartById: async function (id) {
-        return await cartModel.findById(id).populate("user").populate("items.product");
+        return await cartModel
+            .findById(id)
+            .populate("user")
+            .populate("items.product");
     },
 
-    // 3. Lấy tất cả cart theo username
+    // 3. Lấy tất cả cart theo user
     GetCartByUsername: async function (username) {
-        let user = await userModel
-            .findOne({
-                username: username,
-            });
-        let cart = await cartModel.findOne({ user: user._id }).populate("items.product");
+        let user = await userModel.findOne({
+            username: username,
+        });
+        let cart = await cartModel
+            .findOne({ user: user._id })
+            .populate("items.product");
         return cart;
     },
     // 4. Lấy cart theo userId
     GetCartByUserId: async function (userId) {
         return await cartModel
             .findOne({ user: userId })
-            .populate("items.product")
+            .populate("items.product_id")
             .populate("user");
-    },    
+    },
     // 5. Tạo cart mới
     CreateACart: async function (data, user) {
         try {
             // Tính tổng giá từ các sản phẩm
             let total = 0;
             for (const item of data.items) {
-                const product = await productModel.findById(item.product);
+                const product = await productModel.findById(item.product_id);
                 if (!product) throw new Error("Product not found");
                 total += item.quantity * product.sold;
             }
@@ -61,9 +68,8 @@ module.exports = {
             cart.items = data.items;
             let total = 0;
             for (const item of cart.items) {
-                const product = await productModel.findById(item.product);
-                if (!product)
-                    throw new Error("Product not found");
+                const product = await productModel.findById(item.product_id);
+                if (!product) throw new Error("Product not found");
                 total += item.quantity * (product.sold || 0);
             }
 
@@ -84,5 +90,5 @@ module.exports = {
         } catch (error) {
             throw new Error(error.message);
         }
-    }
+    },
 };
